670042aa3c20cfd960d4ede4cee0128fe97f0bcd
r4sso <r4sso@localhost.localdomain>
2021-09-24 23:46:56 +0800

    Initial Commit

--- parrot-os/.config/polybar/launch.sh
+++ parrot-os/.config/polybar/launch.sh
@@ -1,5 +1,14 @@
-#!/bin/bash
-
-
+#!/usr/bin/env sh
+# Terminate already running bar instances
 killall -q polybar
-polybar parrot
+
+# Wait until the processes have been shut down
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload main &
+  done
+else
+  polybar --reload main &
+fi
